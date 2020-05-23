e93b9a1a767577c2d7d383203177b0d6fa7d6db0
cronree91 <you.reetok.tube@gmail.com>
2020-05-23 22:49:44 +0900

    fix formatter

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ load_dotenv()
 TOKEN = os.environ["TOKEN"]
 servers_db = TinyDB('servers.json')
 groups = {}
-formatter = "%(levelname)s %(asctime)s %(levelname)s %(funcName)s %(lineno)d %(message)s"
+formatter = "%(levelname)s %(asctime)s %(funcName)s %(lineno)d %(message)s"
 logging.basicConfig(filename='logs/logger.log',format=formatter,level=logging.DEBUG)
 client = discord.Client()
 
